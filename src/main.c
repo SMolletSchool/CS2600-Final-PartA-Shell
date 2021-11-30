12a821689c8a2a8243f37fda1d98302eb331a47b
SMolletSchool <shmollet@cpp.edu>
2021-11-29 19:15:35 -0800

    Implemented line reading

--- src/main.c
+++ src/main.c
@@ -1,4 +1,42 @@
 
+char *lsh_read_line()
+{
+    #define BUFFER_SIZE 1024
+    int buffer_size = BUFFER_SIZE;
+    int position = 0;
+    char *buf = malloc(sizeof(char) * buffer_size);
+    int c;
+
+    if (!buf) {
+        fprintf(stderr, "failed to allocate, uh oh\n"); //Prints this to standard error, 2>
+        exit(1); //exit with status 1, EXIT_FAILURE
+    }
+
+    while (1) { //Infinite loop on purpose
+        //Read
+        c = getchar();
+
+        //For end of file
+        if (c == EOF || c == '\n') {
+            buf[position] = c;
+            return buf;
+        } else {
+            buf[position] = c;
+        }
+        position++;
+
+        //For buffer exceeding
+        if (position >= buffer_size) {
+            buffer_size += BUFFER_SIZE;
+            buf = realloc(buf, buf);
+            if (!buf) {
+                fprintf(stderr, "failed to reallocate, uh oh spaghettios\n");
+                exit(1);
+            }
+        }
+    }
+}
+
 void lsh_loop()
 {
     char *line;
