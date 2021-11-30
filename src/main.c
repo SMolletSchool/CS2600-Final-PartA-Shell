06457716419faf50e6399ed8dd69b56d29d9cfa4
SMolletSchool <shmollet@cpp.edu>
2021-11-29 19:04:04 -0800

    Set up basic loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,28 @@
+
+void lsh_loop()
+{
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("input here: ");
+        line = lsh_read_line(line);
+        args = lsh_split_line(args);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
 int main (int argc, char **argv)
 {
     //Load
+
     //Run
+    lsh_loop();
     //Shutdown
+    
     return 0; //EXIT_SUCESS, VSC throws fit as it doesn't exist on pi
 }
