6fa66f304e11a0dfc327c4d16c9bd0d6a9bdfcff
SMolletSchool <shmollet@cpp.edu>
2021-11-29 19:38:14 -0800

    importing stdlib.h has helped, implemented getline version

--- src/main.c
+++ src/main.c
@@ -1,40 +1,19 @@
+#include <stdlib.h>
+
 
 char *lsh_read_line()
 {
-    #define BUFFER_SIZE 1024
-    int buffer_size = BUFFER_SIZE;
-    int position = 0;
-    char *buf = malloc(sizeof(char) * buffer_size);
-    int c;
-
-    if (!buf) {
-        fprintf(stderr, "failed to allocate, uh oh\n"); //Prints this to standard error, 2>
-        exit(1); //exit with status 1, EXIT_FAILURE
-    }
-
-    while (1) { //Infinite loop on purpose
-        //Read
-        c = getchar();
-
-        //For end of file
-        if (c == 0 || c == '\n') { //if c is EOF, which is 0
-            buf[position] = c;
-            return buf;
+    ssize_t buffer_size = 0;
+    char *line = NULL;
+    if (getline(&line, &buffer_size, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);
         } else {
-            buf[position] = c;
-        }
-        position++;
-
-        //For buffer exceeding
-        if (position >= buffer_size) {
-            buffer_size += BUFFER_SIZE;
-            buf = realloc(buf, buf);
-            if (!buf) {
-                fprintf(stderr, "failed to reallocate, uh oh spaghettios\n");
-                exit(1);
-            }
+            perror("readline");
+            exit(EXIT_FAILURE);
         }
     }
+    return line;
 }
 
 void lsh_loop()
@@ -62,5 +41,5 @@ int main (int argc, char **argv)
     lsh_loop();
     //Shutdown
     
-    return 0; //EXIT_SUCESS, VSC throws fit as it doesn't exist on pi
+    return EXIT_SUCCESS;
 }
